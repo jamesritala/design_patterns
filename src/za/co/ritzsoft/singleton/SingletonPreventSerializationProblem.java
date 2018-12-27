285eff8b1c499d92f4699638da0131648bb4f271
jamesritala <jamesritala@gmail.com>
2018-12-27 18:23:34 +0200

    Added Singleton implementation that solves the serialization/deserialization issue

---
+++ src/za/co/ritzsoft/singleton/SingletonPreventSerializationProblem.java
@@ -0,0 +1,25 @@
+package za.co.ritzsoft.singleton;
+
+import java.io.Serializable;
+
+public class SingletonPreventSerializationProblem implements Serializable {
+
+    private static SingletonPreventSerializationProblem instance = new SingletonPreventSerializationProblem();
+
+    private SingletonPreventSerializationProblem(){}
+
+    public static SingletonPreventSerializationProblem getInstance(){
+        return instance;
+    }
+
+    /*
+    *
+    * When we serialize an object and deserialize it again, there are different hashcode values generated . So the sngleton principle breaks in this case.
+    * To fix the issue we implement the readResolve method
+    * */
+
+    private Object readResolve(){
+        System.out.println("Applying read resolve....");
+        return instance;
+    }
+}
