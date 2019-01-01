ccc6849ef084404db93876c6428ddc7bcedee690
jamesritala <jamesritala@gmail.com>
2019-01-01 18:47:14 +0200

    Added a fun easter egg: Guessing game

---
+++ src/za/co/ritzsoft/funeasteregg/GuessingGame.java
@@ -0,0 +1,24 @@
+package za.co.ritzsoft.funeasteregg;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessingGame {
+
+    public static void main(String args[]){
+
+        System.out.println("I'm thinking of a number between 1 and 5. What number am I thinking of?\n");
+        int guess=0;
+        int computerGeneratedNumber= 0;
+        Random rand= new Random();
+        computerGeneratedNumber=rand.nextInt(5);
+        //System.out.println("Random: "+computerGeneratedNumber);
+        Scanner scan= new Scanner(System.in);
+        do{
+            guess= scan.nextInt();
+            if(guess!=computerGeneratedNumber)
+                System.out.println("Wrong! Try again. Enter another number below....\n");
+        }while(guess!=computerGeneratedNumber);
+        System.out.println("You finally got it. I was thinking of "+computerGeneratedNumber);
+    }
+}
